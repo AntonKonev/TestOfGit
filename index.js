f0d88ae279f35d257dd5ba2afe42acdf581f183f
AntonKonev <akonev1993@gmail.com>
2018-01-12 14:04:36 +0200

    Third commit add second subsription

--- index.js
+++ index.js
@@ -53,4 +53,10 @@ observable.subscribe({
     error: err => console.error('something wrong occurred: ' + err),
     complete: () => console.log('done'),
 });
+observable.subscribe({
+    next: x => console.log('got value ' + x),
+    error: err => console.error('something wrong occurred: ' + err),
+    complete: () => console.log('done'),
+});
+
 console.log('just after subscribe');
